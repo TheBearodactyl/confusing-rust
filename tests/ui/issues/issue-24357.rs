6e797263c1a0fe7816b27d5c90f378be5f57756b
The Motherfucking Bearodactyl <gannonkoenig@gmail.com>
2024-04-22 10:15:29 -0500

    init for_dummies branch

---
+++ tests/ui/issues/issue-24357.rs
@@ -0,0 +1,11 @@
+struct NoCopy; //~ NOTE if `NoCopy` implemented `Clone`, you could clone the value
+fn main() {
+   let x = NoCopy;
+   //~^ NOTE move occurs because `x` has type `NoCopy`
+   let f = move || { let y = x; };
+   //~^ NOTE value moved into closure here
+   //~| NOTE variable moved due to use in closure
+   let z = x;
+   //~^ ERROR use of moved value: `x`
+   //~| NOTE value used here after move
+}
